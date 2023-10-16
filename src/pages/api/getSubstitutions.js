5c842230f0a115f043412564d68eb95e7355327b
Majek_PL <kacekkaka@gmail.com>
2023-10-16 18:53:35 +0200

    api for getting substitutions by timetable app (+ cheerio rewrite)

--- src/pages/api/getSubstitutions.js
+++ src/pages/api/getSubstitutions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { parse } from "node-html-parser";
+import {load} from "cheerio";
 
 export default async (req, res) => {
   try {
@@ -7,51 +7,90 @@ export default async (req, res) => {
       "http://kristofc.webd.pro/plan/InformacjeOZastepstwach.html"
     );
 
-    const document = parse(response.data);
-    const time = document.querySelector("h2").textContent.trim();
-    const tables = Array.from(document.querySelectorAll("table")).map(
-      (table) => {
-        const tableObject = {
-          time: table.querySelector("tr:first-child").textContent.trim(),
-          zastepstwa: Array.from(table.querySelectorAll("tr"))
-            .slice(1)
-            .map((row) => {
-              const [
-                lesson,
-                teacher,
-                branch,
-                subject,
-                classValue,
-                caseValue,
-                message,
-              ] = Array.from(row.querySelectorAll("td")).map((cell) =>
-                cell.textContent.trim()
-              );
-              return {
-                lesson,
-                teacher,
-                branch,
-                subject,
-                class: classValue,
-                case: caseValue,
-                message,
-              };
-            }),
-        };
+    const $ = load(response.data);
+    const time = $("h2").text().trim();
+    const tables = [];
 
-        tableObject.zastepstwa = tableObject.zastepstwa.filter(
-          (obj) => JSON.stringify(obj) !== JSON.stringify({})
-        );
-        tableObject.zastepstwa.sort((a, b) => a.lesson - b.lesson);
-        return tableObject;
-      }
-    );
+    $("table").each((index, table) => {
+      const rows = $(table).find("tr");
+      const zastepstwa = [];
 
-    const form = { time, tables };
+      rows.slice(1).each((i, row) => {
+        const columns = $(row).find("td");
+        const [
+          lesson,
+          teacher,
+          branch,
+          subject,
+          classValue,
+          caseValue,
+          message,
+        ] = columns.map((index, column) => $(column).text().trim()).get();
 
-    res.status(200).json(form);
+        if (lesson) {
+          if (req?.query?.search && req?.query?.query) {
+            switch (req?.query?.search) {
+              case "teacher":
+                if (teacher?.toLowerCase()?.includes(req?.query?.query?.toLowerCase())) {
+                  zastepstwa.push({
+                    lesson,
+                    teacher,
+                    branch,
+                    subject,
+                    class: classValue,
+                    case: caseValue,
+                    message,
+                  });
+                }
+                break
+              case "branch":
+                if (branch?.toLowerCase()?.includes(req?.query?.query?.toLowerCase())) {
+                  zastepstwa.push({
+                    lesson,
+                    teacher,
+                    branch,
+                    subject,
+                    class: classValue,
+                    case: caseValue,
+                    message,
+                  });
+                }
+                break
+              default:
+                zastepstwa.push({
+                  lesson,
+                  teacher,
+                  branch,
+                  subject,
+                  class: classValue,
+                  case: caseValue,
+                  message,
+                });
+            }
+          } else {
+            zastepstwa.push({
+              lesson,
+              teacher,
+              branch,
+              subject,
+              class: classValue,
+              case: caseValue,
+              message,
+            });
+          }
+        }
+      });
+
+      tables.push({
+        time: rows.first().text().trim(),
+        zastepstwa: zastepstwa,
+      });
+    });
+
+    res.status(200).json({time, tables});
+
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Something went wrong" });
+    res.status(500).json({error: "Something went wrong"});
   }
 };
